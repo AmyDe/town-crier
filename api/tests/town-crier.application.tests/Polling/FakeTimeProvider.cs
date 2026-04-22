dcff78db80218f8a934e970a448f4ba5ce8653b0
AmyDe <amy@salter.uk>
2026-04-22 11:18:10 +0100

    test(polling): make FakeTimeProvider advanceable and hook page fetch

--- api/tests/town-crier.application.tests/Polling/FakeTimeProvider.cs
+++ api/tests/town-crier.application.tests/Polling/FakeTimeProvider.cs
@@ -1,6 +1,18 @@
 namespace TownCrier.Application.Tests.Polling;
 
-internal sealed class FakeTimeProvider(DateTimeOffset utcNow) : TimeProvider
+internal sealed class FakeTimeProvider : TimeProvider
 {
-    public override DateTimeOffset GetUtcNow() => utcNow;
+    private DateTimeOffset utcNow;
+
+    public FakeTimeProvider(DateTimeOffset utcNow)
+    {
+        this.utcNow = utcNow;
+    }
+
+    public override DateTimeOffset GetUtcNow() => this.utcNow;
+
+    public void Advance(TimeSpan delta)
+    {
+        this.utcNow += delta;
+    }
 }
