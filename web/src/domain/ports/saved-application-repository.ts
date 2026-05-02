e76c8f205a2ffbfd50b8ef793d6485ccb1e90b35
AmyDe <amy@salter.uk>
2026-05-02 12:23:30 +0100

    red: useSavedApplication.toggleSave accepts full PlanningApplication (tc-xdhh)

--- web/src/domain/ports/saved-application-repository.ts
+++ web/src/domain/ports/saved-application-repository.ts
@@ -1,7 +1,7 @@
-import type { ApplicationUid, SavedApplication } from '../types';
+import type { ApplicationUid, PlanningApplication, SavedApplication } from '../types';
 
 export interface SavedApplicationRepository {
   listSaved(): Promise<readonly SavedApplication[]>;
-  save(uid: ApplicationUid): Promise<void>;
+  save(application: PlanningApplication): Promise<void>;
   remove(uid: ApplicationUid): Promise<void>;
 }
